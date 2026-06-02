05c919ddc7611229fa69261a311bc2f77354aa3e
vovaskypolyakov-rgb <vova.sky.polyakov@gmail.com>
2026-06-02 21:22:23 +0300

    Commit

--- src/test/java/SickLeaveTest.java
+++ src/test/java/SickLeaveTest.java
@@ -34,6 +34,7 @@ public class SickLeaveTest {
         options.addPreference("pdfjs.disabled", true);
 
         driver = new FirefoxDriver(options);
+
         driver.manage().window().maximize();
         wait = new WebDriverWait(driver, Duration.ofSeconds(15));
 
